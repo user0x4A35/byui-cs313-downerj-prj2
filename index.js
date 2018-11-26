01697fd38376b3e502d792d6f317603dc8816024
downerj <downer.james.256k@gmail.com>
2018-11-26 00:30:39 -0700

    Week10/Prj2: Add sqlDB + endpoints (bare)

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+// external imports
+const express = require('express');
+const path = require('path');
+
+// local imports
+const endpoints = require('./modules/endpoints.js');
+
+// constants
+const PORT = process.env.PORT || 5000;
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+    .use(express.static(path.join(__dirname, 'public')))
+    .set('views', path.join(__dirname, 'views'))
+    .set('view engine', 'ejs')
+    // static pages
+    .get('/', (req, res) => res.sendFile('index.html'))
+    // endpoint: chip list
+    .put('/chiplist', () => {})
+    .post('/chiplist', () => {})
+    .get('/chiplist', () => {})
+    // endpoint: player
+    .put('/player', () => {})
+    .post('/player', () => {})
+    .get('/player', () => {})
+    // endpoint: auth
+    .put('/auth', () => {})
+    .post('/auth', () => {})
+    .get('/auth', () => {})
+    // endpoint: others (404)
+    .use(() => {})
+    .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+// for reference: render EJS
+// <express()>.get('/', (req, res) => res.render('pages/index'))
