13198ea9db2e619c0ee244068839508681944158
user0x4a35 <dow16002@byui.edu>
2018-12-12 23:03:40 -0700

    Week12/Prj2: viewchips.js: url -> filename

--- public/scripts/viewchips.js
+++ public/scripts/viewchips.js
@@ -11,7 +11,7 @@ $(document).ready(() => {
 
                 let imgCell = tableRow.insertCell();
                 let img = constructElement('IMG', {
-                    href: `/assets/images/chips/${dataRow.url}`,
+                    href: `/assets/images/chips/${dataRow.filename}`,
                     alt: dataRow.name,
                 });
                 img.style.width = '100px';
