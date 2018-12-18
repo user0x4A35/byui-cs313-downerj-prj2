6649aca12eed74ce6626f791fefa76b7c8cc01d9
user0x4a35 <dow16002@byui.edu>
2018-12-17 18:07:18 -0700

    Week13/Prj2: Add sorting to library table

--- public/scripts/library.js
+++ public/scripts/library.js
@@ -1,69 +1,121 @@
-let dataRows = {};
+let dataRows = [];
+let thead = $('#tbl-chip-list > thead')[0];
+let tbody = $('#tbl-chip-list > tbody:last-child')[0];
+const theadToKeyMap = {
+    'ID': 'id',
+    'Name': 'name',
+    'Dmg': 'damage',
+    'Mem': 'memory',
+    'Rarity': 'rarity',
+    'Elem': 'element',
+    'Type': 'chiptype',
+};
+let imageElems = {};
 
 $(document).ready(() => {
     $.get({
         url: '/library/chiplist',
         dataType: 'json',
         success: (data, textStatus, jqXHR) => {
-            let tbody = $('#tbl-chip-list > tbody:last-child')[0];
-            let dataRows = data.rows;
+            dataRows = Object.keys(data.rows).map((key) => {
+                return data.rows[key];
+            });
 
-            for (let key in dataRows) {
-                let dataRow = dataRows[key];
-                let tableRow = tbody.insertRow();
-
-                let imgCell = tableRow.insertCell();
-                let img = constructElement('IMG', {
-                    src: `./assets/images/chips/${dataRow.filename}`,
-                    alt: '—',
-                    title: dataRow.name,
-                });
-                img.style.width = '64px';
-                img.style.height = '56px';
-                imgCell.appendChild(img);
-
-                for (let key in dataRow) {
-                    let value = dataRow[key];
-
-                    switch (key) {
-                        case 'filename':
-                            continue;
-                        case 'codes':
-                            value = value
-                                .replace(/[\{\}]*/g, '')
-                                .split(/\,\s*/)
-                                .sort()
-                                .join('');
-                            break;
-                        case 'id':
-                            if (value >= 300 && value < 400) {
-                                value = `M&mdash;${value - 300}`;
-                            } else if (value >= 400) {
-                                value = `G&mdash;${value - 400}`;
-                            } else {
-                                value = `S&mdash;${value}`;
-                            }
-                            break;
-                        case 'rarity':
-                            value = parseInt(value);
-                            break;
-                        case 'chiptype':
-                            if (value === 'STANDARD') {
-                                value = 'STND';
-                            }
-                            break;
-                    }
-
-                    let cell = tableRow.insertCell();
-                    if (key === 'codes') {
-                        cell.style.fontFamily = 'monospace';
-                    }
-                    cell.innerHTML = (value) ? value : '&mdash;';
-                } 
-            }
+            getImages(dataRows);
+            populateTable(dataRows);
         },
         error: (jqXHR, textStatus, errorThrown) => {
             console.error(errorThrown);
         },
     });
 });
+
+function clearTable() {
+    while (tbody.rows.length > 0) {
+        tbody.rows[0].remove();
+    }
+}
+
+function getImages(dataRows) {
+    for (let dataRow of dataRows) {
+        let img = constructElement('IMG', {
+            src: `./assets/images/chips/${dataRow.filename}`,
+            alt: '—',
+            title: dataRow.name,
+        });
+        img.style.width = '64px';
+        img.style.height = '56px';
+        imageElems[dataRow.id] = img;
+    }
+}
+
+function populateTable(dataRows) {
+    for (let dataRow of dataRows) {
+        let tableRow = tbody.insertRow();
+
+        let imgCell = tableRow.insertCell();
+        imgCell.appendChild(imageElems[dataRow.id]);
+
+        for (let key in dataRow) {
+            let value = dataRow[key];
+
+            switch (key) {
+                case 'filename':
+                    continue;
+                case 'codes':
+                    value = value
+                        .replace(/[\{\}]*/g, '')
+                        .split(/\,\s*/)
+                        .sort()
+                        .join('');
+                    break;
+                case 'id':
+                    if (value >= 300 && value < 400) {
+                        value = `M&mdash;${value - 300}`;
+                    } else if (value >= 400) {
+                        value = `G&mdash;${value - 400}`;
+                    } else {
+                        value = `S&mdash;${value}`;
+                    }
+                    break;
+                case 'rarity':
+                    value = parseInt(value);
+                    break;
+                case 'chiptype':
+                    if (value === 'STANDARD') {
+                        value = 'STND';
+                    }
+                    break;
+            }
+
+            let cell = tableRow.insertCell();
+            if (key === 'codes') {
+                cell.style.fontFamily = 'monospace';
+            }
+            cell.innerHTML = (value) ? value : '&mdash;';
+        } 
+    }
+}
+
+function sortTableBy(key) {
+    dataRows.sort((lhs, rhs) => {
+        if (lhs[key] > rhs[key]) {
+            return +1;
+        } else if (lhs[key] < rhs[key]) {
+            return -1;
+        } else {
+            return 0;
+        }
+    });
+
+    let theadRow = thead.rows[0];
+    for (let cell of theadRow.cells) {
+        if (theadToKeyMap[cell.innerHTML] === key) {
+            cell.classList.add('u-th-selected');
+        } else {
+            cell.classList.remove('u-th-selected');
+        }
+    }
+    clearTable();
+    populateTable(dataRows);
+}
